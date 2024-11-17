cb0b2ffa43da1bb2311b08537effe43cf1ec9df9
ismail <153764937+ismzrm@users.noreply.github.com>
2024-11-17 18:42:08 +0300

    CS_QR_CODE_GENERATOR
    C# QR CODE PROJECT

---
+++ C# QR code generator/2024.10.15_QR_KOD/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace _2024._10._15_QR_KOD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 34);
+            this.textBox1.MaxLength = 40;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "KAREKOD YARAT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(184, 149);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "KAYDET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(269, 357);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ C# QR code generator/2024.10.15_QR_KOD/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+
+namespace _2024._10._15_QR_KOD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() != "")
+            {
+                QRCodeEncoder turet = new QRCodeEncoder();
+                pictureBox1.Image = turet.Encode(textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("AŞAĞIYA BİR KAYNAK VERİSİ YAZINIZ");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("QR kod oluşturunuz!!");
+                return;
+            }
+            var resim = pictureBox1.Image;
+            resim.Save(Application.StartupPath + "\\" + DateTime.Now.ToShortDateString() +
+                DateTime.Now.ToFileTime() + ".png");
+            MessageBox.Show("QR CODE başarıyla kaydedildi!!");
+            pictureBox1.Image = null;
+            textBox1.Text = "";
+            
+        }
+    }
+}
